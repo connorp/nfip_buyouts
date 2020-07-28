cabd63134b61cb3f44b4bbbe9dd5273058b2ac78
Connor P. Jackson <cpjackson@berkeley.edu>
2020-07-28 12:30:22 -0700

    start exploratory code

---
+++ NFIP_exploration.R
@@ -0,0 +1,9 @@
+## Connor P. Jackson second year paper
+## cpjackson@berkeley.edu
+## Exploratory code: FEMA National Flood Insurance Program
+
+library(data.table)
+library(httr)
+library(jsonlite)
+
+
