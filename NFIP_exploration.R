84cc18d6df63aaa122087d1d6300d9615a424663
Connor P. Jackson <cpjackson@berkeley.edu>
2020-07-29 14:44:23 -0700

    Retry failed queries, parallelize JSON conversion
    Check which queries failed and try them again, with a limit to the number of retries.
    
    Parallelize the conversion of the JSON data to data.frames

--- NFIP_exploration.R
+++ NFIP_exploration.R
@@ -6,11 +6,13 @@ library(data.table)
 library(jsonlite)
 library(httr)
 library(crul)
+library(parallel)
 
 policies_path <- "https://www.fema.gov/api/open/v1/FimaNfipPolicies"
 claims_path <- "https://www.fema.gov/api/open/v1/FimaNfipClaims"
 
-query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery = 1000, queryCap = NULL) {
+query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery = 1000,
+                           queryCap = NULL, times = 5) {
   # First find out how long the data are
   initial_resp <- httr::GET(url = API_url,
                             query = list(`$inlinecount` = 'allpages',
@@ -36,12 +38,36 @@ query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery =
   }
 
   # Assemble a vector of URLs with the same criteria but per page
-  query_starts <- seq(from = 0, to = N, by = perQuery)
-  query_urls <- sapply(query_starts, assemble_url)
+  page_indices <- seq(from = 0, to = N, by = perQuery)
+  query_urls <- sapply(page_indices, assemble_url)
 
   # Query the URLs asynchronously
   Async_client <- Async$new(urls = query_urls)
-  query_results <- Async_client$get()
+
+  query_results <- list()
+
+  for (i in 1:times) {
+    # a retry loop for getting failed pages of the API
+
+    # Send the GET request
+    query_responses <- Async_client$get()
+
+    # Identify the queries that succeeded
+    query_success <- sapply(query_responses, function(z) z$success())
+
+    # save the successful queries
+    query_results <- append(query_results, query_responses[query_success])
+
+    # Identify the failed queries, prepare to reissue them
+    query_urls <- query_urls[!query_success]
+    Async_client$urls <- query_urls
+
+    # We are done if no queries failed
+    if (length(query_urls) == 0) break
+
+    # otherwise, wait an exponential decay time (with jitter) and retry the failed ones
+    Sys.sleep(stats::runif(1, min = 1, max = min(2^i, 60)))
+  }
 
   parse_query <- function(query_result) {
     query_outputs <- jsonlite::fromJSON(query_result$parse("UTF-8"))
@@ -50,6 +76,6 @@ query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery =
     return(query_outputs[[tablename]])
   }
 
-  outtable <- rbindlist(lapply(query_results, parse_query), fill = TRUE)
+  outtable <- rbindlist(mclapply(query_results, parse_query, mc.cores = 3), fill = TRUE)
   return(outtable)
 }
