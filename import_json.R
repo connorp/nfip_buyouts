7572937df666850947171ed7df0f55da3b393481
Connor P. Jackson <cpjackson@berkeley.edu>
2020-10-28 18:45:43 -0700

    Finalize script for CSV conversion of policies data

--- import_json.R
+++ import_json.R
@@ -3,13 +3,16 @@
 library(jsonlite)
 library(data.table)
 
+# Fix the column names and their order by reading the first line of data
+toprow <- fromJSON(readLines("../NFIP_data/policies_actual_ND.json", n=1))
+col_names <- names(toprow)
+
+# Start a CSV file with column headers and nothing else
+write(paste(col_names, collapse = ","), "../NFIP_data/policies.csv")
+
 stream_csv <- function(X) {
-  Xtab <- fromJSON(as.character(X))
-  fwrite(xtab, "policies.csv", append=TRUE)
+  fwrite(X[, col_names], "../NFIP_data/policies.csv", append=TRUE)
 }
 
-colfile <- fromJSON(file("policies_first_record.json"))
-names(colfile)
-
-policies_json <- stream_in(file("fimanfippolicies.json"),
-                           handler = stream_csv, pagesize = 100000)
+stream_in(file("../NFIP_data/policies_actual_ND.json"),
+          handler = stream_csv, pagesize = 100000)
