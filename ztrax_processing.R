37b564fd7cb80d0fe2d922d29d4cfd9cb2174ee9
Connor P. Jackson <cpjackson@berkeley.edu>
2020-11-04 09:21:36 -0800

    build processing of ZTRAX data

---
+++ ztrax_processing.R
@@ -0,0 +1,22 @@
+## Connor P. Jackson second year paper
+## cpjackson@berkeley.edu
+## ZTRAX data import and processing
+
+library(data.table)
+library(demogztrax)
+
+## ---- import-assessment ----
+
+ZAsmt_struct <- demogztrax::layout_spec$Zillow_Assessor.ZAsmt
+
+cols_main <- c(1,4,5,27,28,30,37,82,83)
+colnames_main <- ZAsmt_struct[TableName == "utMain" & column_id %in% cols_main, FieldName]
+
+cols_building <- c(1,15)
+colnames_bldg <- ZAsmt_struct[TableName == "utBuilding" & column_id %in% cols_building, FieldName]
+
+colnames_value <- ZAsmt_struct[TableName == "utValue", FieldName]
+colnames_all <- unique(c(colnames_main, colnames_bldg, colnames_value))
+
+nchomes <- ztrax_fread(branch="Zasmt",state="NC", keeptabs = c("utMain", "utBuilding", "utValue"),
+                       keepvars = colnames_all)
