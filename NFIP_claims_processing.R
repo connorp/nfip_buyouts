bf118feef1433822d4faa600b78ee5d5f5efc6e6
Connor P. Jackson <cpjackson@berkeley.edu>
2020-11-04 16:28:36 -0800

    Import data with functions, work on census tracts

--- NFIP_claims_processing.R
+++ NFIP_claims_processing.R
@@ -9,22 +9,24 @@ library(sf)
 
 assemble_claims <-function(path="../data_buyouts/FimaNfipClaims.csv", state=NULL) {
   claims <- fread(path, stringsAsFactors = TRUE)
+  state_filter <- state
 
   validclaims <-
     claims[occupancyType == 1 & totalBuildingInsuranceCoverage > 0 &
              totalBuildingInsuranceCoverage <= 250000 & amountPaidOnBuildingClaim > 0 &
              amountPaidOnBuildingClaim <= 250000 &
              amountPaidOnBuildingClaim <= totalBuildingInsuranceCoverage]
-  if (is.null(state)) {
+  if (is.null(state_filter)) {
     return(validclaims)
   } else {
-    return(validclaims[state == ..state])
+    return(validclaims[state == state_filter])
   }
 }
 
 claims <- assemble_claims(state="NC")
 
+
 ## ---- import-NFHL-zones ----
 
 # nfhl_layers <- st_layers("../nfip_data/NFHL/NFHL_48_20201014.gdb")
-nfhl <- sf::st_read("../nfip_data/NFHL/NFHL_48_20201014.gdb", "S_Fld_Haz_Ar")
+# nfhl <- sf::st_read("../nfip_data/NFHL/NFHL_48_20201014.gdb", "S_Fld_Haz_Ar")
--- ztrax_processing.R
+++ ztrax_processing.R
@@ -4,11 +4,12 @@
 
 library(data.table)
 library(demogztrax)
+library(tigris)
 
 ## ---- import-ztrax-assessment ----
 ZAsmt_struct <- demogztrax::layout_spec$Zillow_Assessor.ZAsmt
 
-cols_main <- c(1,4,5,27,28,30,37,82,83)
+cols_main <- c(1,4,5,27,28,30,82,83)
 colnames_main <- ZAsmt_struct[TableName == "utMain" & column_id %in% cols_main, FieldName]
 
 cols_building <- c(1,15)
@@ -17,8 +18,18 @@ colnames_bldg <- ZAsmt_struct[TableName == "utBuilding" & column_id %in% cols_bu
 colnames_value <- ZAsmt_struct[TableName == "utValue", FieldName]
 colnames_all <- unique(c(colnames_main, colnames_bldg, colnames_value))
 
-nchomes <- ztrax_fread(branch="Zasmt",state="NC", keeptabs = c("utMain", "utBuilding", "utValue"),
-                       keepvars = colnames_all)
+file_names <- c("utMain", "utBuilding", "utValue")
+
+read_assessment <- function(state, files=NULL, colnames=NULL){
+  nchomes <- ztrax_fread(branch="Zasmt",state=state, keeptab=files,
+                         keepvars = colnames)
+
+  lapply(nchomes, function(table) setkey(table, RowID, FIPS, BatchID))
+  return(Reduce(function(...) merge(..., all = T), nchomes))
+}
+
+nchomes <- read_assessment("NC", file_names, colnames_all)
+nctracts <- tracts(37)
 
 # Manually import some data?
 # nc <- fread("~/ZTRAX/clean/2019/Zillow_Assessor/37/ZAsmt/Main.txt",
