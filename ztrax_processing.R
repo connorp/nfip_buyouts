6b20377c8298d5ea6645a28217a99f9587411523
Connor P. Jackson <cpjackson@berkeley.edu>
2020-11-08 10:16:43 -0800

    Select and describe sample of parcel data

--- ztrax_processing.R
+++ ztrax_processing.R
@@ -8,10 +8,10 @@ library(demogztrax)
 ## ---- import-ztrax-assessment ----
 ZAsmt_struct <- demogztrax::layout_spec$Zillow_Assessor.ZAsmt
 
-cols_main <- c(1:3,5,27,28,30,32,33,82:84)
+cols_main <- c(1:3,5:8,27,28,30,82:84)
 colnames_main <- ZAsmt_struct[TableName == "utMain" & column_id %in% cols_main, FieldName]
 
-cols_building <- c(1:6,9,11,13,15,16,46)
+cols_building <- c(1,6,15,16,46)
 colnames_bldg <- ZAsmt_struct[TableName == "utBuilding" & column_id %in% cols_building, FieldName]
 
 cols_value <- c(1:9,14)
@@ -20,15 +20,26 @@ colnames_all <- unique(c(colnames_main, colnames_bldg, colnames_value))
 
 file_names <- c("utMain", "utBuilding", "utValue")
 
-read_assessment <- function(state, files=NULL, colnames=NULL){
-  nchomes <- ztrax_fread(branch="Zasmt",state=state, keeptab=files,
-                         keepvars = colnames)
+nchomes <- ztrax_fread(branch="Zasmt",state="NC", keeptab=file_names,
+                       keepvars = colnames_all)
 
-  lapply(nchomes, function(table) setkey(table, RowID, FIPS))
-  return(Reduce(function(...) merge(..., all = T), nchomes))
-}
+for (table in names(nchomes)) setkey(nchomes[[table]], RowID, FIPS)
 
-nchomes <- read_assessment("NC", file_names, colnames_all)
+nchomes <- Reduce(function(...) merge(..., all = T), nchomes)
+
+home_types <- c("RR000","RR101","RR102","RR103","RR104","RR105","RR106","RR107",
+                "RR108","RR109","RR110","RR111","RR112","RR113","RR114","RR115",
+                "RR116","RR117","RR118","RR119","RR120")
+
+nchomes <- nchomes[PropertyLandUseStndCode %in% home_types]
+
+# Identify the most recent assessment records
+nchomes[, latest := (.I[which.max(Edition)] == .I), by = .(ImportParcelID)]
+
+# Handle empty strings
+nchomes[PropertyFullStreetAddress == "", PropertyFullStreetAddress := NA]
+nchomes[PropertyCity == "", PropertyCity := NA]
+nchomes[PropertyAddressCensusTractAndBlock == "", PropertyAddressCensusTractAndBlock := NA]
 
 fwrite(nchomes, "../data_buyouts/ZAsmt_NC.csv")
 
@@ -39,4 +50,5 @@ fwrite(nchomes, "../data_buyouts/ZAsmt_NC.csv")
 ## ---- import-ztrax-transactions ----
 Ztrans_struct <- demogztrax::layout_spec$Zillow_Transaction.ZTrans
 
+# nctrans <- ztrax_fread("ZTranTran", state="NC", keeptabs = "utMain")
 
