2e0c988ec902db716c594f6fc6cbc63923a7ed60
Connor P. Jackson <cpjackson@berkeley.edu>
2020-11-08 10:54:46 -0800

    Import and merge transaction data

--- ztrax_processing.R
+++ ztrax_processing.R
@@ -50,5 +50,19 @@ fwrite(nchomes, "../data_buyouts/ZAsmt_NC.csv")
 ## ---- import-ztrax-transactions ----
 Ztrans_struct <- demogztrax::layout_spec$Zillow_Transaction.ZTrans
 
-# nctrans <- ztrax_fread("ZTranTran", state="NC", keeptabs = "utMain")
+cols_trans <- c(1,2,5,7,17:20,25,27:32,60:63,66,68,102)
+colnames_trans <- Ztrans_struct[TableName == "utMain" & column_id %in% cols_trans, FieldName]
+colnames_trans <- c(colnames_trans, "ImportParcelID")
+trans_files <- c("utMain", "utPropertyInfo")
 
+nctrans <- ztrax_fread("ZTranTran", state="NC", keeptabs = trans_files,
+                       keepvars = colnames_trans)
+
+for (table in names(nctrans)) setkey(nctrans[[table]], TransId, FIPS)
+
+nctrans <- merge(nctrans$Main.txt, nctrans$PropertyInfo.txt, all.x = TRUE)
+
+nctrans <- nctrans[ImportParcelID %in% nchomes[, ImportParcelID]]
+# Will need to identify transactions that lack ImportParcelID
+
+fwrite(nctrans, "../data_buyouts/ZTrans_NC.csv")
