01561b82d4e9f8e9a97be67c940ad2722cc48d10
Connor P. Jackson <cpjackson@berkeley.edu>
2020-11-15 19:08:02 -0800

    improved API query code with error checking

--- NFIP_exploration.R
+++ NFIP_exploration.R
@@ -1,6 +1,6 @@
 ## Connor P. Jackson second year paper
 ## cpjackson@berkeley.edu
-## Exploratory code: FEMA National Flood Insurance Program
+## Query the OpenFEMA API for data in CSV format, and write it out to a file
 
 library(data.table)
 library(httr)
@@ -10,10 +10,12 @@ policies_path <- "https://www.fema.gov/api/open/v1/FimaNfipPolicies"
 claims_path <- "https://www.fema.gov/api/open/v1/FimaNfipClaims"
 
 
-query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery = 1000,
-                           queryCap = NULL, queryN = FALSE) {
-  if (queryN == TRUE) {
-    # First find out how long the data are
+query_FEMA_API <- function(API_url, outfile, columns = NULL, rowfilter = NULL,
+                           perQuery = 1000, N = NULL) {
+  # For a given API path, query the API for CSV data, passing parameters
+
+  if (is.null(N)) {
+    # Query the API for the length of the data
     initial_resp <- httr::GET(url = API_url,
                               query = list(`$inlinecount` = 'allpages',
                                            `$top` = 1,
@@ -21,13 +23,6 @@ query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery =
     parsed_result <- jsonlite::fromJSON(httr::content(initial_resp, as = "text"))
     N <- parsed_result$metadata$count
     print(N)
-  } else{
-    N <- 1629168
-  }
-
-  # If we don't want to pull the full table
-  if (!is.null(queryCap)) {
-    N <- min(N, queryCap)
   }
 
   # Build the vector of URLs to query
@@ -51,17 +46,22 @@ query_FEMA_API <- function(API_url, columns = NULL, rowfilter = NULL, perQuery =
   col_names <- names(toptable)
 
   # Start a CSV file with column headers and nothing else
-  write(paste(col_names, collapse = ","), "../data_buyouts/policies.csv")
+  write(paste(col_names, collapse = ","), outfile)
 
   query_and_read <- function(url) {
+    # query a URL, get the results, and append it to the CSV
     query_resp <- httr::GET(url)
+    if (http_error(query_resp)) {
+      return(c("Failure: ", url))
+    }
     # Sys.sleep(stats::runif(1, min = 1, max = min(2^i, 60)))
     res_table <- fread(text=httr::content(query_resp, as = "text"), na.strings = "")
-    fwrite(res_table[, ..col_names], "../data_buyouts/policies.csv", append=TRUE)
+    fwrite(res_table[, ..col_names], outfile, append=TRUE)
   }
 
   lapply(query_urls, query_and_read)
 }
 
-query_FEMA_API(policies_path, rowfilter = "propertyState eq 'NC'")
+query_FEMA_API(policies_path, "../data_buyouts/policies.csv",
+               rowfilter = "propertyState eq 'NC'", N = 1629168)
 
